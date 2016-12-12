8412399c07713ac15b89e928e9bbf3d35ae4dc2e
Sasha Voynow <sasha@labzero.com>
2016-12-12 12:59:18 -0800

    remove litter

--- lib/pull_request_responder.ex
+++ lib/pull_request_responder.ex
@@ -14,19 +14,16 @@ defmodule HedwigGithub.PullRequestResponder do
   github-prs <org>/<repo> - display the last 5 open pull requests for the repo 
   """
   hear ~r/^github-prs (?<org>.*)\/(?<repo>.*)/i, message do
-    IO.puts("received PR request")
     org = message.matches["org"]
     repo = message.matches["repo"]    
     send_messages(Helper.pull_requests(org, repo), message)    
   end
 
   def send_messages(prs, message) when length(prs) > 0 do
-    IO.puts("have prs")
     Enum.each(prs, fn commit -> send message, commit end)    
   end
 
   def send_messages(_, message) do
-    IO.puts("don't have prs")
     send message, "No open PRs for that repo"
   end
 end
