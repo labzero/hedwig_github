2e8c40298f0188fb7bf4345104af00a34d0c12ff
Brien Wankel <brien@reebosak.net>
2016-12-15 10:09:31 -0700

    Refactor Responder commands
    Improve documentation just a bit

---
+++ lib/responder.ex
@@ -0,0 +1,37 @@
+defmodule HedwigGithub.Responder do
+  @moduledoc """
+  Fetches the last 5 commits for a Github repo
+
+  It requires a github access token, configured like so.
+  
+  config :hedwig_github, :github_api_token, "your_access_token"
+
+  """
+  use Hedwig.Responder
+  alias HedwigGithub.Helper
+
+  @usage """
+  github activity <org>/<repo> - display the last 5 commits for the repo 
+  github prs <org>/<repo> - display the last 5 open pull requests for the repo 
+  """
+  hear ~r/^github activity (?<org>.*)\/(?<repo>.*)/i, message do
+    org = message.matches["org"]
+    repo = message.matches["repo"]
+    Enum.each(Helper.commits(org, repo), fn commit -> send message, commit end)    
+  end
+
+  hear ~r/^github prs (?<org>.*)\/(?<repo>.*)/i, message do
+    org = message.matches["org"]
+    repo = message.matches["repo"]    
+    send_messages(Helper.pull_requests(org, repo), message)    
+  end
+
+  def send_messages(prs, message) when length(prs) > 0 do
+    Enum.each(prs, fn commit -> send message, commit end)    
+  end
+
+  def send_messages(_, message) do
+    send message, "There are currently no open pull requests."
+  end
+
+end
--- test/pull_request_responder_test.exs
+++ test/pull_request_responder_test.exs
@@ -2,9 +2,9 @@ defmodule PullRequestResponderTest do
   use ExUnit.Case
   use Hedwig.RobotCase
 
-  @tag start_robot: true, name: "clippy", responders: [{HedwigGithub.PullRequestResponder, []}]
+  @tag start_robot: true, name: "clippy", responders: [{HedwigGithub.Responder, []}]
   test "github-prs displays pull requests", %{adapter: adapter, msg: msg} do
-    send adapter, {:message, %{msg | text: "github-prs labzero/lunch"}}
+    send adapter, {:message, %{msg | text: "github prs labzero/lunch"}}
     Enum.each(0..1, fn _ -> 
       assert_receive {:message, %{text: text}}, 5000
       assert String.contains?(text, "Author:")
