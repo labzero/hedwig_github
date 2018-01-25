214c098e2c78811947a4d64d9af1f539a05f32c8
Brien Wankel <brien@reebosak.net>
2018-01-25 12:37:43 -0700

    Update dependencies

--- lib/responder.ex
+++ lib/responder.ex
@@ -3,7 +3,7 @@ defmodule HedwigGithub.Responder do
   Fetches the last 5 commits for a Github repo
 
   It requires a github access token, configured like so.
-  
+
   config :hedwig_github, :github_api_token, "your_access_token"
 
   """
@@ -11,23 +11,23 @@ defmodule HedwigGithub.Responder do
   alias HedwigGithub.Helper
 
   @usage """
-  github activity <org>/<repo> - display the last 5 commits for the repo 
-  github prs <org>/<repo> - display the last 5 open pull requests for the repo 
+  github activity <org>/<repo> - display the last 5 commits for the repo
+  github prs <org>/<repo> - display the last 5 open pull requests for the repo
   """
   hear ~r/^github activity (?<org>.*)\/(?<repo>.*)/i, message do
     org = message.matches["org"]
     repo = message.matches["repo"]
-    Enum.each(Helper.commits(org, repo), fn commit -> send message, commit end)    
+    Enum.each(Helper.commits(org, repo), fn commit -> send message, commit end)
   end
 
   hear ~r/^github prs (?<org>.*)\/(?<repo>.*)/i, message do
     org = message.matches["org"]
-    repo = message.matches["repo"]    
-    send_messages(Helper.pull_requests(org, repo), message)    
+    repo = message.matches["repo"]
+    send_messages(Helper.pull_requests(org, repo), message)
   end
 
   def send_messages(prs, message) when length(prs) > 0 do
-    Enum.each(prs, fn commit -> send message, commit end)    
+    Enum.each(prs, fn commit -> send message, commit end)
   end
 
   def send_messages(_, message) do
--- test/support/test_github_api.ex
+++ test/support/test_github_api.ex
@@ -3,14 +3,14 @@ defmodule HedwigGithub.TestGithubApi do
   @behaviour HedwigGithub.ApiClient
 
   def commits(_org, _repo, _limit) do
-    {:ok, fake_commits}
+    {:ok, fake_commits()}
   end
 
   def pull_requests(_org, _repo, _limit) do
-    {:ok, fake_prs}
+    {:ok, fake_prs()}
   end
 
-  def shorten_url(_url) do      
+  def shorten_url(_url) do
     "http://example.com/short"
   end
 
@@ -18,16 +18,16 @@ defmodule HedwigGithub.TestGithubApi do
     [
       fake_commit("alice", "abcd", "fixed stuff"),
       fake_commit("bob", "1234", "broke stuff")
-    ]     
+    ]
   end
 
   defp fake_prs do
-    [ 
+    [
       fake_pr("alice", "change all the stuff"),
       fake_pr("bob", "rewrite everything")
     ]
   end
-  
+
   defp fake_commit(login, sha, message) do
     %{"author" => %{"login" => login}, "sha" => sha, "html_url" => "http://example.com/#{sha}", "commit" => %{"message" => message}}
   end
